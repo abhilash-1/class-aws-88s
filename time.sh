e78f1353492a06cae9358cd8a1d5d2a432612d1c
abhilash <abhilashbunny89@gmail.com>
2026-02-04 23:07:32 +0530

    execution time

--- time.sh
+++ time.sh
@@ -1,14 +1,14 @@
 
 Start_time=$(date+%s)
 
-echo "The start time is $Start_time "
+echo "The start time is: $Start_time "
  
 sleep 10
 
 End_time=$(date+$s)
 
-echo "The end time is $End_time"
+echo "The end time is: $End_time"
 
-Tot_time=($End_time-$Start_time)
+Tot_time=$(($End_time-$Start_time))
 
-echo "the time took to execute is $Tot_time"
+echo "the time took to execute is : $Tot_time"
