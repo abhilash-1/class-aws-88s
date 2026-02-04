691ef606b07f6475eb62215b9909312b7261be5b
abhilash <abhilashbunny89@gmail.com>
2026-02-04 22:13:42 +0530

    Dynamic variable

--- var.sh
+++ var.sh
@@ -1,7 +1,9 @@
 #!bin/bash
-
-echo "Luffy: Gomu Gomu na pistollluu"
-echo "Luffy: eat..."
-echo "Naruto: talk no jutsu"
-echo "Naruto: Ramen"
-echo "Sanji: sake..."
+$A="Luffy"
+$B="Naruto"
+$C="Sanji"
+echo "$A: Gomu Gomu na pistollluu"
+echo "$A: eat..."
+echo "$B: Ero Senseii"
+echo "$B: Ramen"
+echo "$C: sake..."
