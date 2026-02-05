78bca42c0643012efba1f9549af5aed775ba9a37
abhilash <abhilashbunny89@gmail.com>
2026-02-05 22:46:10 +0530

    installation (exit status as shell won't be exited automatically)

--- installation.sh
+++ installation.sh
@@ -7,7 +7,7 @@ dnf install nginx -y
 User_id = $(id -u)
 
 
-if [$User_id -ne 0]; then
+if [ $User_id -ne 0 ]; then
     echo " you are not a root user, please proceed with sudo access"
     exit 1
 fi
@@ -15,7 +15,7 @@ fi
 echo "installing nginx"
 dnf install nginx -y
 
-if ($? -ne 0); then
+if [ $? -ne 0 ]; then
     echo "Instllation failed...nginx"
     exit 1
 else
