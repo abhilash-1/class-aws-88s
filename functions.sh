3b40c59e1f7e87c0496a74b55599682b543e02cb
abhilash <abhilashbunny89@gmail.com>
2026-02-05 23:20:35 +0530

    Functions usage

---
+++ functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Functions in shell scripting"
+
+validate()
+    if [ $1 -ne 0 ]; then
+        echo "$2: installation failed"
+    else 
+        echo "$2: installation successfull"
+    fi
+
+dnf install nginx -y
+validate $? "Nginx"
+dnf install nodejs -y
+validate $? "Nodejs"
+dnf install redis -y
+validate $? "Redis"
