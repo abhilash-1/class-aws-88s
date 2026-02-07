b9550139cc194fe1e202a0fe58bf5758a7f85d3f
abhilash <abhilashbunny89@gmail.com>
2026-02-07 15:22:17 +0530

    using trap to know where the script failed at which line and which command

---
+++ trap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+trap "Script failed at line number $LINENO,and at Command:$BASH_COMMAND" ERR
+
+echo "yo mann"
+echo "hi dudeee"
+echoo "how are"
+echo "what man , what's going on"
