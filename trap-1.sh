296cd11a1bf173ccda3c295d39da74ba74a094bd
abhilash <abhilashbunny89@gmail.com>
2026-02-07 15:42:27 +0530

    using trap to know where the script failed at which line and which command-1

--- trap-1.sh
+++ trap-1.sh
@@ -13,34 +13,34 @@ echo "$R using for loop to execute multiple commands and $G making it flexible s
 User_id=$(id -u)
 
 if [ $User_id -ne 0 ]; then 
-    echo "Please get the root access...or if u already have the root access use root user by ...sudo su"
+    echo -e "$Y Please get the root access...or if u already have the root access use root user by ...sudo su"
     exit 1
 fi
 
 
-Logs_folder=$"/var/logs/shell-script"
-Logs_file=$"/var/logs/shell-script/"$0".log"
+Logs_folder="/var/log/shell-script"
+Logs_file="/var/log/shell-script/"$0".log"
 
-echo "Creating directory in /var/logs path"
+echo -e "$Y Creating directory in /var/logs path"
 
 mkdir -p "$Logs_folder"
 
-trap ' There is an error at line number $LINENO, and command: $BASH_COMMAND | tee -a "Logs_file" ' ERR
+trap ' echo "There is an error at line number $LINENO, and command: $BASH_COMMAND" | tee -a "Logs_file" ' ERR
 
 validate(){
     set +e
     if [ $1 -ne 0 ]; then
         
-        echo "$R $2: Installation falied" | tee -a $Logs_file
+        echo -e "$R $2: Installation falied" | tee -a $Logs_file
     else
-        echo "$G $2: Installation success" | tee -a $Logs_file
+        echo -e "$G $2: Installation success" | tee -a $Logs_file
     fi
     set -e
 }
 
 for package in "$@" # it will take all args passed by the user and store each of them in package and iterate
 do
-    echo "Installing :$package"
+    echo -e "$G Installing :$package"
     dnf install $package -y &>> $Logs_file
     validate $? "$package"
 done
