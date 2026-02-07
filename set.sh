a37f4004a90c891f2697e0f9202ea667852e3605
abhilash <abhilashbunny89@gmail.com>
2026-02-07 14:28:54 +0530

    using set to exit when err found

---
+++ set.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+echo "for loop"
+
+echo "using for loop to execute multiple commands and making it flexible so it can accept any number of arguments"
+
+User_id=$(id -u)
+
+if [ $User_id -ne 0 ]; then 
+    echo "Please get the root access...or if u already have the root access use root user by ...sudo su"
+    exit 1
+fi
+
+Logs_folder=$"/var/logs/shell-script"
+Logs_file=$"/var/logs/shell-script/"$0".log"
+
+echo "Creating directory in /var/logs path"
+
+mkdir -p $Logs_folder
+
+validate(){
+    set +e
+    if [ $1 -ne 0 ]; then
+        
+        echo "$2: Installation falied" | tee -a $Logs_file
+    else
+        echo "$2: Installation success" | tee -a $Logs_file
+    fi
+    set -e
+}
+
+for package in "$@" # it will take all args passed by the user and store each of them in package and iterate
+do
+    set +e
+    echo "Installing :$package"
+    dnf install $package -y &>> $Logs_file
+    validate $? "$package"
+    set -e
+done
