4bc937a2df15a95634a163ac00a56d7e7cd8947d
abhilash <abhilashbunny89@gmail.com>
2026-02-08 20:58:15 +0530

    Installing the package using shell scripting

---
+++ mongo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Installing Mongod in this server to set up DB"
+
+User_id=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+Logs_folder="/var/logs/shell-script"
+Logs_file="$Logs_folder/$0.log"
+
+
+if [ $User_id -ne 0 ]; then
+    echo -e " $R please get sudo/root access to make changes here....if u laready have access use "sudo su" to get into root directory"
+    exit 1
+else
+    echo -e " $G You are in root directory or you have sudo access...so proceed forward"
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo " $R $2: .....FAILED"
+    else
+        echo "$G $2:....SUCCESS"
+    fi
+}
+
+echo "Creating Logs_folder in /var/logs/"
+mkdir -p "$Logs_folder"
+validate $? "Folder Creation"
+
+for package in "$@"
+do
+
+    echo "Installing $package"
+    echo "Before Installing we arechecking if its already installed or not"
+    dnf list installed $package -y
+    if [ $? -eq 0 ]; then
+        echo -e "$Y The $package is already installed so...skipping it..."
+    else
+        dnf install "$package" -y
+        validate $? "Nginx Installation"
+
+done
