07eac0e450fdffa6fc18e42152e40a7f604e8bd8
abhilash <abhilashbunny89@gmail.com>
2026-02-09 15:37:57 +0530

    Installing mongo and starting the server

--- mongo.sh
+++ mongo.sh
@@ -54,17 +54,12 @@ do
         validate $? "$package Installation"
         if [ $? -eq 0 ]; then
             echo "Starting the $package server using SYSTEMCTL"
-            sytemctl start mongod &>> Logs_file
+            sytemctl start mongod &>>$Logs_file
             validate $? "Started the server"
-            systemctl enable mongod &>> Logs_file
+            systemctl enable mongod &>>$Logs_file
             validate $? "enabled the server"
-            systemctl status mongod &>> Logs_file
-            if [ $? -eq 0 ]; then
-                echo -e " $G The server is running fine ...."
-            else
-                echo -e "$R There is an error while starting the server sp, check the logs"
-                exit 1
-            fi
+            systemctl status mongod &>>$Logs_file
+            validate $? "The status of the server is"
         else
             echo -e "$R There is an issue with installation...or while installing the appliaction"
             exit 1
