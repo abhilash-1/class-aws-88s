6cd460e9362981ff143f7e34e8d9f11f0c7e43d4
abhilash <abhilashbunny89@gmail.com>
2026-02-14 17:23:31 +0530

    migrating older files

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source ./common.sh
+
+R='\[e31m'
+G='\[e32m'
+Y='\[e33m'
+
+
+source="/home/ec2-user/app-logs"
+destination="/var/log/backup"
+
+echo -e " $Y moving the files from $source to $destination"
+
+LogsFileCreation
+User_id
+
+echo "$Y Finding the files older than 14 days, and zipping them and moving to new location"
+
+Files_del=$(find $source -name "*.log" -mtime +14 &>>$Logs_file )
+if [ -z "$Files_del" ]; then
+    echo -e " $R There are no files older than 14 days...So skipping"
+else
+    while IFS= read -r filepath; do
+        echo -e " $Y Reading the filepath of logs : $filepath "
+        echo -e " $G Moving the file path to destination : $destination "
+        mv $source $destination &>>$Logs_file 
+        validate $? "The migration of older log files is....."
+        echo -e " $G The new path of older log files: $destination"
+
+fi
